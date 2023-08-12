bcd4ae544861c48716b166582efd046abb2bbbb7
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-12 09:35:25 -0300

    Create index.js

---
+++ FrontEnd/screens/index.js
@@ -0,0 +1,23 @@
+import Search from "./Search/Search";
+import Home from "./Home/Home";
+import Profile from "./Profile/Profile";
+import Cart from "./Cart";
+import ProductDetails from "./ProductDetails/ProductDetails";
+import NewRivals from "./NewRivals/NewRivals";
+import Login from "./Login/Login";
+import Favorites from "./Favorites/Favorites";
+import Orders from "./Orders/Orders";
+import Registration from "./Registration/Registration";
+
+export {
+    Home,
+    Search,
+    Profile,
+    Cart,
+    ProductDetails,
+    NewRivals,
+    Login,
+    Favorites,
+    Orders,
+    Registration
+}

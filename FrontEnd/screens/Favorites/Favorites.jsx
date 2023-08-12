e9fceccd8a6dae9eb7afa01ee15f0ce3e2ac5ff0
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-12 09:35:21 -0300

    Create Favorites.jsx

---
+++ FrontEnd/screens/Favorites/Favorites.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+import styles from './Favorites.style';
+
+const Favorites = () => {
+  return (
+    <View>
+      <Text>Favorites</Text>
+    </View>
+  )
+}
+
+export default Favorites;

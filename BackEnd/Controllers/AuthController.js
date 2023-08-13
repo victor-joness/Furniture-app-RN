2bf61c3116dcbe8b35e1eba11b4ad53ae02f2b30
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-13 18:11:03 -0300

    Create AuthController.js

---
+++ BackEnd/Controllers/AuthController.js
@@ -0,0 +1,56 @@
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  createUser: async (req, res) => {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      location: req.body.location,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString(),
+    });
+
+    try {
+      await newUser.save();
+      res.status(201).json({ message: "User Cretion Successfull" });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  },
+
+  loginUser: async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+
+      console.log(user);
+
+      if (!user) {
+        return res.status(401).json("Wrong Credentials");
+      }
+
+      const decryptPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.PASS_SEC
+      );
+      const decryptPass = decryptPassword.toString(CryptoJS.enc.Utf8);
+
+      if (decryptPass !== req.body.password) {
+        return res.status(401).json("Wrong Credentials");
+      }
+
+      const userToken = jwt.sign({ id: user._id }, process.env.JWT_SEC, {
+        expiresIn: "7d",
+      });
+
+      const { password, __v, createdAt, updatedAt, ...userData } = user._doc;
+
+      res.status(200).json({ ...userData, token: userToken });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  },
+};

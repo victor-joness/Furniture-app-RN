342e6644e0d9968d9188ed1ba1efcfec4b257f14
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-13 17:57:56 -0300

    Create Search.style.js

---
+++ FrontEnd/screens/Search/Search.style.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../constants/index";
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.medium,
+    marginVertical: SIZES.medium,
+    height: 50,
+    marginHorizontal: SIZES.small,
+  },
+  searchIcon: {
+    marginHorizontal: SIZES.xSmall,
+    color: COLORS.gray,
+    marginTop: SIZES.small,
+    /* lineHeight: 50, */
+  },
+  searchWrapper: {
+    flex: 1,
+    backgroundColor: COLORS.secondary,
+    marginRight: SIZES.small,
+    borderRadius: SIZES.small,
+  },
+  searchInput: {
+    fontFamily: "regular",
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: SIZES.small,
+  },
+  searchImage: {
+    resizeMode: "contain",
+    width: SIZES.width - 80,
+    height: SIZES.height - 200,
+    opacity: 0.9,
+  },
+  searchBtn: {
+    width: 50,
+    height: "100%",
+    borderRadius: SIZES.medium,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.primary,
+  },
+});
+
+export default styles;

e8955974589ba781c204481d3e8e72e7e95a5044
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-13 17:58:09 -0300

    Create ProductDetails.style.js

---
+++ FrontEnd/screens/ProductDetails/ProductDetails.style.js
@@ -0,0 +1,126 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../constants";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  upperRow: {
+    marginHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "absolute",
+    top: SIZES.xxLarge,
+    width: SIZES.width - 44,
+    zIndex: 999,
+  },
+  image: {
+    aspectRatio: 1,
+    resizeMode: "cover",
+  },
+  details: {
+    marginTop: -SIZES.large,
+    backgroundColor: COLORS.lightWhite,
+    width: SIZES.width,
+    borderTopLeftRadius: SIZES.medium,
+    borderTopRightRadius: SIZES.medium,
+  },
+  titleRow: {
+    marginHorizontal: 20,
+    paddingBottom: SIZES.xLarge,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: SIZES.width - 44,
+    top: 20,
+  },
+  ratingRow: {
+    paddingBottom: SIZES.small,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: SIZES.width - 10,
+    top: 5,
+  },
+  rating: {
+    top: SIZES.large,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginHorizontal: SIZES.large,
+  },
+  ratingText: {
+    color: COLORS.gray,
+    fontSize: SIZES.medium,
+    paddingHorizontal: SIZES.xSmall,
+  },
+  descriptionWrapper: {
+    marginTop: SIZES.large * 2,
+    marginHorizontal: SIZES.large,
+  },
+  description: {
+    fontFamily: "medium",
+    fontSize: SIZES.large - 2,
+  },
+  descText: {
+    fontFamily: "regular",
+    fontSize: SIZES.small,
+    textAlign: "justify",
+    marginBottom: SIZES.small,
+  },
+  location: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: COLORS.secondary,
+    padding: 5,
+    marginHorizontal: SIZES.small,
+    borderRadius: SIZES.large,
+  },
+  cartTitle: {
+    fontFamily: "semiBold",
+    marginLeft: SIZES.small,
+    fontSize: SIZES.medium,
+    color: COLORS.lightWhite,
+  },
+  cartRow: {
+    paddingBottom: SIZES.small,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: SIZES.width ,
+  },
+  cartBtn: {
+    width: SIZES.width * 0.7,
+    backgroundColor: COLORS.black,
+    padding: SIZES.small/2,
+    borderRadius: SIZES.large,
+    marginLeft: 12
+  },
+  addCartBtn: {
+    width: 37,
+    height: 37,
+    borderRadius: 50,
+    margin: SIZES.small,
+    backgroundColor: COLORS.black,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontFamily: "bold",
+    fontSize: SIZES.large,
+  },
+  price: {
+    paddingHorizontal: 10,
+    fontFamily: "semiBold",
+    fontSize: SIZES.large,
+    paddingTop: 3,
+  },
+  priceWrapper: {
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.large
+  },
+});
+
+export default styles;

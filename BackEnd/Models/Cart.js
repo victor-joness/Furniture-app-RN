adfd485b476d06c1b63f05e49c27b035410730c7
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-14 08:17:05 -0300

    Create Cart.js

---
+++ BackEnd/Models/Cart.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        cartItem: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: { type: Number, defalut: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", CartSchema);

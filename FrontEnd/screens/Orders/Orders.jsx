9c48a7a122ca522d6a45ec4c324d4c868d778e0c
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-14 08:16:48 -0300

    Create Orders.jsx

---
+++ FrontEnd/screens/Orders/Orders.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+import styles from './Orders.style';
+
+const Orders = () => {
+  return (
+    <View>
+      <Text>Orders</Text>
+    </View>
+  )
+}
+
+export default Orders;

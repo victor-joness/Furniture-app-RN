c908895e11192cfc44cb2eeb37bafb5d82becefd
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-26 08:28:33 -0300

    Create Headings.style.js

---
+++ FrontEnd/components/Headings/Headings.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+import {COLORS, SIZES} from "../../constants/index";
+
+const styles = StyleSheet.create({
+    container : {
+        marginTop: SIZES.medium,
+        marginHorizontal: 12
+    },
+    header : {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    headerTitle : {
+        fontFamily: "semiBold",
+        fontSize: SIZES.xLarge -2
+    }
+});
+
+export default styles;

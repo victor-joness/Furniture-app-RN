40b3be844e6b2b705f60ce5eca8086d631bbfbb1
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-26 08:28:31 -0300

    Create index.js

---
+++ FrontEnd/components/index.js
@@ -0,0 +1,17 @@
+import Welcome from './Welcome/Welcome';
+import ProductCartView from './products/ProductCartView/ProductCartView';
+import ProductRow from './products/ProductRow/ProductRow';
+import ProductList from './products/ProductList/ProductList';
+import SearchTile from './SearchTile/SearchTile';
+import BackBtn from './backBtn/backBtn';
+import Button from './Button/Button';
+
+export {
+    Welcome,
+    ProductCartView,
+    ProductRow,
+    ProductList,
+    SearchTile,
+    BackBtn,
+    Button
+}

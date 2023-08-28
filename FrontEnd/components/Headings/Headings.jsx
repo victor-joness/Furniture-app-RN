3d99c42ebb9b1043e4c5adca9f5e81ba21be32a4
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-08-28 08:17:02 -0300

    Create Headings.jsx

---
+++ FrontEnd/components/Headings/Headings.jsx
@@ -0,0 +1,25 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+import { useNavigation } from "@react-navigation/native";
+import styles from "./Headings.style";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../../constants";
+
+const Headings = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>New Rivals</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("ProductList")}>
+            <Ionicons name="ios-grid" size={24} color={COLORS.primary}/>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Headings;
